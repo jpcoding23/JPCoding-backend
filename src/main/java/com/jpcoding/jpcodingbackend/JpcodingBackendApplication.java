a5e7f05806d91dc1f37987b8541579c6ad75eaea
Jonathan Pierre <JonWPierre@gmail.com>
2023-04-27 15:37:34 -0400

    initial code

---
+++ src/main/java/com/jpcoding/jpcodingbackend/JpcodingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jpcoding.jpcodingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpcodingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpcodingBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpcoding/jpcodingbackend/JpcodingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcoding.jpcodingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpcodingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
